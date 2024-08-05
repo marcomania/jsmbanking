c751695bb9d3a22c160e12a7ab8e60d005610eec
marcow85m <marcow85m@gmail.com>
2024-08-05 05:58:32 +0000

    feat: :sparkles: Authentication UI

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server"
+
+export const signIn = async (data: {email: string, password: string}) => {
+  try {
+    //Mutation
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+export const signUp = async (userData : SignUpParams) => {
+  try {
+    //Mutation
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
